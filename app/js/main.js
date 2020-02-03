62a48606fb9ae477b902dddb36169bd1c33d60c1
Roman Lyapaev <XilentDem@gmail.com>
2020-02-03 15:23:18 +0300

    last fix

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,16 @@
-// $(function() {
-// })
+$(function() {
+	var btn = $('.btn-lift-up');
+
+	$(window).scroll(function() {
+	    if ($(window).scrollTop() > 500) {
+	      btn.addClass('show');
+	    } else {
+	      btn.removeClass('show');
+	    }
+	});
+
+    btn.on('click', function(e) {
+	    e.preventDefault();
+	    $('html, body').animate({scrollTop:0}, '300');
+    });
+})
